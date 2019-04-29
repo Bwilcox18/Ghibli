43002b6f841b17d4b9ecc8eedeb42ac3e9b396b0
Bwilcox18 <46695020+Bwilcox18@users.noreply.github.com>
2019-04-29 16:03:00 -0400

    changed scripts

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
-// create a request vairbale and assign a new XMLHttpRequest object to it.
-
 const app = document.getElementById('root');
 
 const logo = document.createElement('img');
-
 logo.src = 'logo.png';
 
 const container = document.createElement('div');
@@ -13,42 +10,32 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-//Open a new connection, ising GET request on the URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 request.onload = function () {
-	
-	//Begin accessing JSON data here
-	var data = JSON.parse(this.response);
-	
-	if (request.status >= 200 && request.status<400) {
-	data.forEach(movie => {
-		//Create a div with a card class
-		const card = document.createElement('div');
-		card.setAttribute('class', 'card');
-		
-		//Create an h1 and set text content to film's title
-		const h1 = document.create.Element('h1');
-		h1.textContent = movie.title;
-		
-		//Create a p and set the text content to the film's description
-		const p = document.createElement('p');
-		movie.description = movie.description.substring(0,300);
-		p.textcontent = `${movie.description)...`;
-		
-		//Append the cards to the container element
-		container.appendChild(card);
-		
-		//Each card will contain an h1 and a p
-		card.appendChild(h1);
-		card.appendChild(p);
-	});
-} else {
-	const errorMessage = document.createElement('marquee');
-	errorMessage.textContent = `Gah, it's not working!`;
-	app.appendChild(errorMessage);
-	
-   }
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+    });
+  } else {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
 }
 
-//send request
 request.send();
